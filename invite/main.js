844a5361a9d0ee7d0ca104a4c6f54d8c9b62ea75
waytPrime <123025850+waytPrime@users.noreply.github.com>
2024-04-24 15:39:05 +0300

    1

--- invite/main.js
+++ invite/main.js
@@ -1,8 +1,9 @@
 const refs = {
   christmasTree: document.querySelector(".container_tree"),
+  forestTree: document.querySelectorAll(".tree"),
 };
 console.log(refs.christmasTree);
-const { christmasTree } = refs;
+const { christmasTree, forestTree } = refs;
 
 // Флаг для отслеживания состояния перемещения
 let isDragging = false;
@@ -34,16 +35,71 @@ function dragObject(event) {
     let deltaY = touch.clientY - initialY;
     christmasTree.style.left = offsetX + deltaX + "px";
     christmasTree.style.top = offsetY + deltaY + "px";
+
     // event.preventDefault();
   }
-}
-
-// Функция для завершения перемещения объекта
-function stopDragging() {
-  isDragging = false;
 }
 
 // Добавляем обработчики событий
 christmasTree.addEventListener("touchstart", startDragging);
 document.addEventListener("touchmove", dragObject);
 document.addEventListener("touchend", stopDragging);
+
+console.log(forestTree);
+
+//
+// Функция для завершения перемещения объекта
+function stopDragging() {
+  isDragging = false;
+  moveChristmasTreeToNearestTree();
+}
+
+// Добавляем обработчик события для christmasTree
+christmasTree.addEventListener("touchend", stopDragging);
+
+// Добавляем обработчики событий для forestTree
+forestTree.forEach((tree) => {
+  tree.addEventListener("touchmove", () => {
+    if (isDragging) {
+      moveChristmasTreeToNearestTree();
+    }
+  });
+});
+
+// Функция для перемещения christmasTree к ближайшему forestTree
+// Функция для перемещения christmasTree к ближайшему forestTree
+// Функция для перемещения christmasTree к ближайшему forestTree
+function moveChristmasTreeToNearestTree() {
+  let closestTree = null;
+  let minDistance = Infinity;
+
+  forestTree.forEach((tree) => {
+    const rectChristmasTree = christmasTree.getBoundingClientRect();
+    const rectTree = tree.getBoundingClientRect();
+
+    const distance = Math.sqrt(
+      Math.pow(rectTree.left - rectChristmasTree.left, 2) +
+        Math.pow(rectTree.top - rectChristmasTree.top, 2)
+    );
+
+    if (distance < minDistance && distance < 50) {
+      minDistance = distance;
+      closestTree = tree;
+    }
+  });
+
+  if (closestTree) {
+    const rectClosestTree = closestTree.getBoundingClientRect();
+    christmasTree.style.left = rectClosestTree.left - 10 + "px"; // Сдвигаем christmasTree влево на 10 пикселей
+    christmasTree.style.top = rectClosestTree.top - 30 + "px"; // Поднимаем christmasTree выше на 30 пикселей
+
+    if (forestTree[0] === closestTree) {
+      redirectToInvitePage();
+    }
+  }
+}
+
+// Функция для перенаправления на страницу invite.html
+function redirectToInvitePage() {
+  window.location.href = "invite1.html";
+}
